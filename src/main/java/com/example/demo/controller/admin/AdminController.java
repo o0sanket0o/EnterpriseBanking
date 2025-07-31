c7826e725e80058281b7b77a2fc79818e8320320
o0sanket0o <sanketchugh5@gmail.com>
2025-07-31 21:42:07 +0530

    Added a logger

--- src/main/java/com/example/demo/controller/admin/AdminController.java
+++ src/main/java/com/example/demo/controller/admin/AdminController.java
@@ -4,12 +4,18 @@ package com.example.demo.controller.admin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 
 @RestController
 @RequestMapping("/admin")
 public class AdminController {
+    private static final Logger logger = LoggerFactory.getLogger(AdminController.class);
+
     @GetMapping("/sayHi")
     public String hiAdmin(){
+        logger.info("GET /admin/sayHi endpoint was called");
         return "Ye mai pehli kar raha hu changes";
     }
 }
